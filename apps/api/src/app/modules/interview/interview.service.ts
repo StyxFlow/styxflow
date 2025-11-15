674d3f96fd9f70032bf49ae6a026ae0eca217ae1
jonayeds <jonayeeds@gmail.com>
2025-11-15 16:46:12 +0600

    feat/added_api_route_to_start_interview

--- apps/api/src/app/modules/interview/interview.service.ts
+++ apps/api/src/app/modules/interview/interview.service.ts
@@ -1,7 +1,7 @@
 import { eq } from "drizzle-orm";
 import { db } from "../../../db/drizzle.js";
 import { getVectorStore } from "../../../db/qdrant.js";
-import { candidate } from "../../../db/schema.js";
+import { candidate, interview } from "../../../db/schema.js";
 import { ApiError } from "../../errors/apiError.js";
 
 const startInterview = async (userId: string) => {
@@ -17,30 +17,23 @@ const startInterview = async (userId: string) => {
     must: [{ key: "metadata.candidateId", match: { value: isCandidate.id } }],
   });
 
-  // console.log(resumeChunks.points.length);
-  // const resumeText = resumeChunks.points
-  //   .sort(
-  //     (a, b) =>
-  //       (a.payload!.metadata! as { candidateId: string; chunkIndex: number })
-  //         .chunkIndex -
-  //       (b.payload!.metadata! as { candidateId: string; chunkIndex: number })
-  //         .chunkIndex
-  //   )
-  //   .map((chunk) => chunk.payload!.content)
-  // .join("\n");
   if (resumeChunks.length === 0) {
     throw new ApiError(404, "No resume found for the candidate");
   }
-  const resumeText = resumeChunks
-    .sort((a, b) => a[0].metadata.chunkIndex - b[0].metadata.chunkIndex)
-    .map((chunk) => chunk[0].pageContent)
-    .join("\n");
-  // const newInterview = await db.insert(interview).values({
-  //   candidateId: isCandidate.id,
-  //   isActive: true,
-  //   attempt: 1,
-  // });
-  return { chunks: resumeChunks, resumeText };
+  // const resumeText = resumeChunks
+  //   .sort((a, b) => a[0].metadata.chunkIndex - b[0].metadata.chunkIndex)
+  //   .map((chunk) => chunk[0].pageContent)
+  //   .join("\n");
+
+  const newInterview = await db
+    .insert(interview)
+    .values({
+      candidateId: isCandidate.id,
+      isActive: true,
+      attempt: 1,
+    })
+    .returning();
+  return { chunks: resumeChunks, newInterview };
 };
 
 export const InterviewService = {
